53e9264954127ab7e98ae7b66f283b6558a57475
zachbuck <zachbuck90@gmail.com>
2021-04-18 20:46:58 -0500

    fixed some stuff and add some testing

--- P-AI.py
+++ P-AI.py
@@ -8,21 +8,27 @@ class controller:
 class layer:
     weights = [] #input, output
 
-    def __init__(self, wieghts):
+    def __init__(self):
         pass
 
     def check(self, inputs):
         #assert that formatting matches
         assert len(inputs) == len(self.weights), "input in controller" + "_" + "at layer" + "_" + "does not match size"
         
-        #create the ouput array
+        #create output array
         output = []
         for _x in range(0, len(self.weights[0])):
             output.append(0)
 
-        #sum the weights 
+        #multiply and add for outputs
         for input in range(0, len(inputs)):
-            for weight in range(0, len(self.weights[input])):
-                output[weight] += input * self.weights[input][weight]
+            for weight in range(0, len(self.weights)):
+                output[weight] += inputs[input] * self.weights[input][weight]
+                # print(str(inputs[input]) + ", " + str(self.weights[input][weight]) + ", " + str(inputs[input] * self.weights[input][weight]))
 
         return output
+
+
+test = layer() 
+test.weights = [[0.25, 0.5], [0.75, 2]]
+print(test.check([1, 2]))
