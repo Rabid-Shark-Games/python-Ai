6def88f189f096c3e7359431e9de85ad0f70c45c
zachbuck <zachbuck90@gmail.com>
2021-04-19 22:05:05 -0500

    controller works now....

--- P-AI.py
+++ P-AI.py
@@ -5,6 +5,28 @@ class controller:
     def __init__(self):
         pass
 
+    def check(self, input):
+        output = []
+
+        for layer in range(0, len(self.layers)):
+
+            #first layer
+            if layer == 0:
+                output = self.layers[layer].check(input)
+            
+            #middle layers
+            elif layer != len(self.layers) - 1:
+                output = self.layers[layer].check(output)
+
+            #last layer
+            else:
+                return self.layers[layer].check(output)
+
+            #debug info
+            print(output)
+
+
+
 class layer:
     weights = [] #input, output
 
@@ -12,8 +34,9 @@ class layer:
         pass
 
     def check(self, inputs):
+
         #assert that formatting matches
-        assert len(inputs) == len(self.weights), "input in controller" + "_" + "at layer" + "_" + "does not match size"
+        assert len(inputs) == len(self.weights), "input in controller " + "TODO" + " at layer " + "TODO" + " does not match size"
         
         #create output array
         output = []
@@ -22,13 +45,24 @@ class layer:
 
         #multiply and add for outputs
         for input in range(0, len(inputs)):
-            for weight in range(0, len(self.weights)):
+            for weight in range(0, len(self.weights[input])):
                 output[weight] += inputs[input] * self.weights[input][weight]
-                # print(str(inputs[input]) + ", " + str(self.weights[input][weight]) + ", " + str(inputs[input] * self.weights[input][weight]))
+                print(str(inputs[input]) + ", " + str(self.weights[input][weight]) + ", " + str(inputs[input] * self.weights[input][weight]))
 
         return output
 
 
 test = layer() 
-test.weights = [[0.25, 0.5], [0.75, 2]]
-print(test.check([1, 2]))
+test.weights = [[2]]
+
+test1 = layer()
+test1.weights = [[1, 0.5]]
+
+test2 = layer()
+test2.weights = [[0.25], [3]]
+
+control = controller()
+control.layers = [test, test1, test2]
+
+print()
+print(control.check([2.0]))
