8dbe802b18682bd8ed84a6ae09ce1efc173bbe98
zachbuck <zachbuck90@gmail.com>
2021-04-20 14:58:28 -0500

    i think some stuff kinda works

--- P-AI.py
+++ P-AI.py
@@ -1,49 +1,7 @@
 import random
 
 
-class layer:
-    pass
-
-
-
-class controller:
-    layers = []
-    rng = random.Random()
-    interation = 0
-
-    def __init__(self, layertype=layer, layersize=[10, 20, 15, 10], seed=0):
-        #initialize random number generator
-        self.rng.seed(seed)
-
-        for layer in range(0, len(layersize)):
-            if not layer == len(layersize) - 2:
-                layertype.generate(seed=self.rng.random(), inp=layersize[layer], out=layersize[layer + 1])
-            else:
-                layertype.generate(seed=self.rng.random(), inp=layersize[layer], out=layersize[-1])
-
-    def check(self, input):
-        output = []
-
-        for layer in range(0, len(self.layers)):
-
-            #first layer
-            if layer == 0:
-                output = self.layers[layer].check(input)
-            
-            #middle layers
-            elif layer != len(self.layers) - 1:
-                output = self.layers[layer].check(output)
-
-            #last layer
-            else:
-                return self.layers[layer].check(output)
-
-            #debug info
-            print(output)
-
-
-
-class layer:
+class Layer:
     weights = [] #input, output
 
     def __init__(self):
@@ -90,19 +48,50 @@ class layer:
 
         #return the temp object
         return temp
+
+
+
+class Controller:
+    layers = []
+    rng = random.Random()
+    interation = 0
+
+    def __init__(self, layertype=Layer, layers=[], seed=0):
+        #initialize random number generator
+        self.rng.seed(seed)
+
+        #handle layer generation
+        if len(layers) == 0: #empty controller
+            pass
+        elif type(layers[0]) == Layer: #pre-created layers
+            for layer in layers:
+                self.layers.append(layer)
+        elif type(layers[0]) == int or type(layers[0]) == float: #generated layers
+            for layer in range(0, len(layers) - 1):
+                self.layers.append(layertype.generate(seed=self.rng.random(), inp=layers[layer], out=layers[layer + 1]))
+
+    def check(self, input):
+        output = []
+
+        for layer in range(0, len(self.layers)):
+
+            #first layer
+            if layer == 0:
+                output = self.layers[layer].check(input)
+            
+            #middle layers
+            elif layer != len(self.layers) - 1:
+                output = self.layers[layer].check(output)
+
+            #last layer
+            else:
+                return self.layers[layer].check(output)
+
+            #debug info
+            print(output)
             
 
 
-test = layer() 
-test.weights = [[2]]
-
-test1 = layer()
-test1.weights = [[1, 0.5]]
-
-test2 = layer()
-test2.weights = [[0.25], [3]]
-
-control = controller(test, test1, test2)
-
-print()
-print(control.check([2.0]))
+test = Controller(layers=[1, 2, 2, 1])
+for x in test.layers:
+    print(x.weights)
