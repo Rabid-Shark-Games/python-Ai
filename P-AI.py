11c76d7bcecdfd1651a88c96f6b625b209dde7c9
zachbuck <zachbuck90@gmail.com>
2021-04-23 12:32:01 -0500

    This is all probably going to get removed later

--- P-AI.py
+++ P-AI.py
@@ -107,6 +107,31 @@ class System:
             for _x in range(0, number):
                 self.controllers.append(copy.deepcopy(controller))
 
+    def check(self, controllers=[], input=[]):
+        if type(controllers) == list: #If Multiple Controllers Are Selected
+            assert len(controllers) == len(input), "Must Give Valid Amount Of Input" #ensure correct amount of inputs
+
+            for x in range(0, len(controllers)):
+                if isinstance(controllers[x], (str, int, float)): #If Value Can Be Cast To Int
+                    controllers[x] = int(controllers[x])
+
+                    assert controllers[x] < len(self.controllers), "Must Choose Valid Controller To Check" #ensure controllers are correctly selected
+                    self.controllers[controllers[x]].check(input[input])
+
+        elif isinstance(controllers, (str, int, float)): #If One Selected And Castable To Int
+            controllers = int(controllers)
+
+            assert controllers < len(self.controllers), "Must Choose Valid Controller To Check" #ensure controllers are correctly selected
+            
+            if type(input[0]) == list:
+                input = input[0] 
+            assert type(input[0]) != list, "Must Format Inputs In A Valid Way"
+            for x in input:
+                assert isinstance(x, (str, int, float)), "Must Use Valid Inputs"
+                
+
+            self.controllers[controllers].check(input)
+
 
 
 test = Controller(layers=[1, 2, 2, 1])
